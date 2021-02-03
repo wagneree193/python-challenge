f474692a511dbbf21351cb938fb20f3c252800b6
wagneree193 <wagneree193@gmail.com>
2021-02-03 16:29:59 -0600

    Used len function to count total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,26 @@ import csv
 
 csvpath = os.path.join("Resources\PyBank_budget_data.csv")
 
+Months = []
+Profits = []
+TotalMonths = []
+Total = [] 
+AverageChange = []
+GreatestIncrease = []
+GreatestDecrease = []
+
 with open(csvpath) as csvfile:
     budget_data = csv.reader(csvpath, delimiter=",")
+    for column in budget_data:
+        Months.append(column[0])
+        
+
+        TotalMonths= len(list(Months))
+
+print(f"Total Months: {TotalMonths}")
     
-    L1=list(budget_data)
-    Months = (len(list(budget_data))  
     
-    
-print(Months) 
+
 
   
 
