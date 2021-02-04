0d4f18c18ceb619648fc6087761216f1e8511b3e
wagneree193 <wagneree193@gmail.com>
2021-02-04 17:13:03 -0600

    Converted profits list to integer and summed

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,20 +8,22 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header=next(csvreader)
  #create lists to store data    
-    months = []
-    profits = []
+    
      
     for column in csvreader:
+        months = []
+        profits = []
 
         months.append(column[0])
-        profits.append(column[1])
+        #convert data from string to integer to sum
+        profits.append(int(column[1]))
 
-        TotalMonths = len(list(months))
+        Months = len(list(months))
         start = profits[0]
-        profits = [profits]
         Total = sum(profits)
 
-    print(f"Total Months: {TotalMonths}")
+    print(f"Total Months: {Months}")
+    print(f"Total: {Total}")      
 
     
     
