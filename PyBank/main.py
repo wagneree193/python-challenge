4c63e568347dc515cd1bf03ac20b6170f39c3b7e
wagneree193 <wagneree193@gmail.com>
2021-02-04 17:19:14 -0600

    calculated average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,11 +8,10 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header=next(csvreader)
  #create lists to store data    
-    
-     
+    months = []
+    profits = [] 
     for column in csvreader:
-        months = []
-        profits = []
+        
 
         months.append(column[0])
         #convert data from string to integer to sum
@@ -21,9 +20,11 @@ with open(csvpath) as csvfile:
         Months = len(list(months))
         start = profits[0]
         Total = sum(profits)
+        Average = (Total/len(list(profits)))
 
     print(f"Total Months: {Months}")
-    print(f"Total: {Total}")      
+    print(f"Total: {Total}")  
+    print(f"Average Change: {Average}")    
 
     
     
